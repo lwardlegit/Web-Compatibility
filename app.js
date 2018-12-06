00bd3aa4d2b6a19d340f688142bd47e36cb1847c
lwardlegit <lwardle@unca.edu>
2018-12-06 15:54:25 -0500

    Update app.js
    updated and functional

--- app.js
+++ app.js
@@ -7,25 +7,39 @@ var ImprovementTips=" ";
 var header = $("header");
 var img = $("img");
 
-alert("ready");
 
-document.addEventListener('DOMContentLoaded', function() {
+
+$(document).ready(function() {
     alert("Ready!");
 
-
-    if(header.class.contains("alt")){
+//CHECK FOR ALT TEXT
+    if($("header:has(alt)")){
       SitePros+="alt text is included in header.";
     }else{
-      ImprovementTips+="You should include alternative text in your header."
+      ImprovementTips+="You should include alternative text in your header.";
     }
 
-    if(img.class.contains("alt")){
+    if($("img:has(alt)")){
       SitePros+="alt text is included in img.";
     }else{
-      ImprovementTips+="You should include alternative text in your images."
+      ImprovementTips+="You should include alternative text in your images.";
+    }
+//CHECK FOR TAB INDEX
+    if($("document:has(tabIndex)")){
+      SitePros+="site has tabing index.";
+    }else{
+      ImprovementTips+="You should add the tabIndex class to your elements.";
     }
 
-    alert("Here's what we liked: "+SitePros+" however, you should work to improve: "+ImprovementTips);
+//CHECK FOR CSS ELEMENTS THAT MIGHT CAUSE VISUAL DISTRACTIONS
+      if($( "body" ).css( "background-color")=== $("p").css("text-color")){
+        ImprovementTips+=" body text and background color should not be the same";
+      }else {
+        SitePros+="the background is not the same color as the text";
+      }
+});
 
-
-}, false);
+$(document).ready(function() {
+  alert("Here's what we liked: "+SitePros);
+  alert("Here's what you can work on "+ImprovementTips);
+});
